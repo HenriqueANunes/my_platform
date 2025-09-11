9f830b9adc3daac25deec6fcc6b433dd47a3885f
HenriqueANunes <henrique.a.nunes@outlook.com>
2025-09-11 17:44:50 -0300

    Corridigo o CustomFutureBuilder;

--- lib/widgets/future_builder.dart
+++ lib/widgets/future_builder.dart
@@ -1,21 +1,30 @@
 import 'package:flutter/material.dart';
 
-Widget futureList(BuildContext context, Future future, Widget returnWidget) {
-  return FutureBuilder(
-    future: future,
-    builder: (context, snapshot) {
-      if (snapshot.connectionState == ConnectionState.waiting) {
-        return const Center(child: CircularProgressIndicator());
-      } else if (snapshot.connectionState == ConnectionState.done) {
-        if (snapshot.hasError) {
-          final error = snapshot.error;
-          return Center(child: Text('$error'));
+class CustomFutureBuilder<T> extends StatelessWidget {
+  final Future<T> future;
+  final Widget Function(BuildContext context, T data) dataBuilder;
+
+  const CustomFutureBuilder({
+    Key? key,
+    required this.future,
+    required this.dataBuilder,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<T>(
+      future: future,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else if (snapshot.hasData) {
+          return dataBuilder(context, snapshot.data!);
         } else {
-          return returnWidget;
+          return const SizedBox.shrink(); // Or a custom empty state widget
         }
-      } else {
-        return const Center(child: Text('future error'));
-      }
-    },
-  );
+      },
+    );
+  }
 }
